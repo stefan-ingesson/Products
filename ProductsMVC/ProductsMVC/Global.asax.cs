28b1feb14c26eb0de411915c5c278df44132b1a7
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-08-31 15:38:42 +0200

    Stor commit... Kan lägga till, ta bort, edit... Start på filtering

--- ProductsMVC/ProductsMVC/Global.asax.cs
+++ ProductsMVC/ProductsMVC/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ProductsMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,8 @@ namespace ProductsMVC
     {
         protected void Application_Start()
         {
+ //           Database.SetInitializer<ProductDbContext>(
+ //new DropCreateDatabaseIfModelChanges<ProductDbContext>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ ProductsMVC/ProductsMVC/Models/ProductListView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsMVC.Models
+{
+    public class ProductListView
+    {
+            public string CurrentCategory { get; set; }    
+    }
+}
