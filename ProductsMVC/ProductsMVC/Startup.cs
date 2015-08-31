1fa701f89c17fc73573403015380724c499effbc
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-08-31 08:24:58 +0200

    Project created

---
+++ ProductsMVC/ProductsMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductsMVC.Startup))]
+namespace ProductsMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
