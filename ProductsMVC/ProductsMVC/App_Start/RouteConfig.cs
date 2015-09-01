45418b5366012a052286de0f1e54dbee118ece65
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-09-01 12:55:10 +0200

    Fixat sidnumrering

--- ProductsMVC/ProductsMVC/App_Start/RouteConfig.cs
+++ ProductsMVC/ProductsMVC/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace ProductsMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
-            );
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Products", action = "List", id = UrlParameter.Optional }
+           );
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
+            //);
 
             routes.MapRoute
                 (
