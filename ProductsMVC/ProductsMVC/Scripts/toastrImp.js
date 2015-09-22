35885507d7585fe637432a8452d91acc2f8a1f79
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-09-22 13:28:29 +0200

    Fixat småpill med success toastr

--- ProductsMVC/ProductsMVC/Scripts/toastrImp.js
+++ ProductsMVC/ProductsMVC/Scripts/toastrImp.js
@@ -7,14 +7,14 @@
 //function displayToastr() {
 //    //alert('yes');
 //    // Display a info toast, with no title
-//    toastr.info('Hi Mahedee, This information for you.')
+//    toastr.info('This information for you.')
 
 //    // Display a warning toast, with no title
-//    toastr.warning('Hi Mahedee, This the first warning for you!')
+//    toastr.warning('This the first warning for you!')
 
 //    // Display a success toast, with a title
-//    toastr.success('Yes! You have successfully completed your task!', 'Congratulation for you, Mahedee!')
+//    toastr.success('Yes! You have successfully completed your task!')
 
 //    // Display an error toast, with a title
-//    toastr.error('An error occured in the solution!', 'Please contact with system administrator.')
+//    toastr.error('An error occured in the solution!')
 //}
